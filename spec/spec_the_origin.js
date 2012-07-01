8e70baea1965948a7d10785d83d2fc0368865ac8
Waylon Calabrese <hiwaylon@gmail.com>
2012-07-01 08:12:28 -0500

    commit before leaving behind for python implementatiob, may be back someday

---
+++ spec/spec_the_origin.js
@@ -0,0 +1,37 @@
+/*
+    The origin of all things. Heirarchy of things is:
+    (TODO: Move to projects wiki.)
+
+    The Origin:
+    -
+        Dimensions:
+        -
+            Galactic Systems:
+            -
+                Galaxy:
+                -
+                    Star Systems:
+                        Star: {}
+                        Planets:
+                        - {}
+        -
+            Holes
+
+    Creating a new Galaxy
+    Pre: The event of creating a galaxy has been determined to exist (probs).
+    (TODO: Fix probability termanology)
+    1. Pick a dimension. Dimensions need probablity of being picked.
+    2. Picj a galactic system.
+    3. Roll the galaxy.
+    4. Add discoverability to the system - how will "scannars" or "radars" know that a new thing exists that
+    can be discovered?
+*/
+var buster = require("buster");
+//var entityWorld = require("../lib/entity_world.js");
+
+buster.spec.expose();
+
+describe("TheOrigin", function() {
+    it("should be the origin of all things", function() {
+    });
+});
